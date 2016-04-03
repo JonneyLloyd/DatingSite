d201325373044a47eb5e56e07b37b6a7ae487d98
JonneyLloyd <jonneylloyd@yahoo.co.uk>
2016-04-03 18:43:56 +0100

    Additions to details page. Likes & dislikes implemented with database.

--- Details.php
+++ Details.php
@@ -1,9 +1,4 @@
 <?php
-
-//need to add another seperate form to add/remove likes & dislikes
-// use a button to bring up a pop up
-
-
 require_once("./include/dbConfig.php");
 include('LogInProcess.php'); // Includes Login Script
 if((isset($_SESSION['login_user'])) && (isset($_SESSION['user_password']))) {
@@ -28,6 +23,66 @@ if((isset($_SESSION['login_user'])) && (isset($_SESSION['user_password']))) {
 
 	$l_name = ucfirst($row[0]);
 
+	$query3 = "SELECT sex, seeking, about from user WHERE user_id = '" . $user_id . "'";
+	$result = mysqli_query($conn, $query3)
+	or die ("Couldn't execute query3.");
+	$row = mysqli_fetch_array($result);
+	$sexMale = "";
+	$sexFemale = "";
+	$seekingMale = "";
+	$seekingFemale = "";
+	$bio = strtolower(htmlspecialchars($row[2]));
+	if ($bio == null)
+		$bio = "Say something about yourself";
+
+	$sex = $row[0];
+	if ($sex == "m")
+		$sexMale = "Checked";
+	else
+		$sexFemale = "Checked";
+	$seeking = $row[1];
+	if ($seeking == "m")
+		$seekingMale = "Checked";
+	else
+		$seekingFemale = "Checked";
+
+	$query = "SELECT like_desc FROM `like` WHERE user_id = '" . $user_id . "'";
+	$result = mysqli_query($conn, $query)
+	or die ("Couldn't execute query.");
+	$counter = 0;
+	$like = array(
+		0    => "",
+		1    => "",
+		2    => "",
+		3    => "",
+		4    => "",
+		5    => ""
+	);
+	while($row = mysqli_fetch_array($result))
+	{
+		$like[$counter] = $row["like_desc"];
+		$counter++;
+		//fix counter offset
+
+
+
+	}
+	$query = "SELECT dislike_desc FROM `dislike` WHERE user_id = '" . $user_id . "'";
+	$result = mysqli_query($conn, $query)
+	or die ("Couldn't execute query.");
+	$counter = 0;
+	$dislike = array(
+		0    => "",
+		1    => "",
+		2    => "",
+		3    => "",
+		4    => ""
+	);
+	while($row = mysqli_fetch_array($result))
+	{
+		$dislike[$counter] = $row["dislike_desc"];
+		$counter++;
+	}
 
 	if (isset($_POST['gender'])) {
 		if (empty($_POST['bio']) || empty($_POST['seeking'])) {
@@ -37,17 +92,61 @@ if((isset($_SESSION['login_user'])) && (isset($_SESSION['user_password']))) {
 		} else {
 			$sex = strtolower(htmlspecialchars($_POST["gender"]));
 			$pref = strtolower(htmlspecialchars($_POST["seeking"]));
+			if ($sex == "m")
+				$sexMale = "Checked";
+			else
+				$sexFemale = "Checked";
+			if ($pref == "m")
+				$seekingMale = "Checked";
+			else
+				$seekingFemale = "Checked";
 			$bio = strtolower(htmlspecialchars($_POST["bio"]));
 			$query1 = "SELECT user_id from user WHERE nickname =  '" . $_SESSION['login_user'] . "';";
 			$result = mysqli_query($conn, $query1)
 			or die ("Couldn't execute query.");
 			$row = mysqli_fetch_array($result);
-
-
 			$query2 = "UPDATE `user` SET `sex` = '" . $sex . "', `seeking` = '" . $pref .
 				"', `about` = '" . $bio . "' WHERE `user`.`user_id` = " . $row[0] . ";";
 			$result = mysqli_query($conn, $query2)
 			or die ("Couldn't execute query2.");
+
+			$like[0]= strtolower(htmlspecialchars($_POST["Like1"]));
+			$like[1]= strtolower(htmlspecialchars($_POST["Like2"]));
+			$like[2]= strtolower(htmlspecialchars($_POST["Like3"]));
+			$like[3]= strtolower(htmlspecialchars($_POST["Like4"]));
+			$like[4]= strtolower(htmlspecialchars($_POST["Like5"]));
+
+
+			$query = "DELETE FROM `like` WHERE user_id = '" . $user_id . "'";
+			$result = mysqli_query($conn, $query)
+			or die ("Couldn't execute delete query.");
+			$query_tail = "VALUES ('". $user_id . "', NULL, '".$like[0]."')";
+			for($i = 1; $i <5; $i++){
+				if ($like != "")
+					$query_tail .= ", ('". $user_id . "', NULL, '" .$like[$i] ."')";
+
+			}
+			$query = "INSERT INTO `like` (`user_id`, `like_id`, `like_desc`)" . $query_tail . ";";
+			$result = mysqli_query($conn, $query)
+			or die ("Couldn't execute insert like query.");
+
+			$dislike[0]= strtolower(htmlspecialchars($_POST["Dislike1"]));
+			$dislike[1]= strtolower(htmlspecialchars($_POST["Dislike2"]));
+			$dislike[2]= strtolower(htmlspecialchars($_POST["Dislike3"]));
+			$dislike[3]= strtolower(htmlspecialchars($_POST["Dislike4"]));
+			$dislike[4]= strtolower(htmlspecialchars($_POST["Dislike5"]));
+
+			$query = "DELETE FROM `dislike` WHERE user_id = '" . $user_id . "'";
+			$result = mysqli_query($conn, $query)
+			or die ("Couldn't execute delete query.");
+			$query_tail = "VALUES ('". $user_id . "', NULL, '".$dislike[0]."')";
+			for($i = 1; $i <5; $i++){
+				if ($like != "")
+					$query_tail .= ", ('". $user_id . "', NULL, '" .$dislike[$i] ."')";
+			}
+			$query = "INSERT INTO `group17db`.`dislike` (`user_id`, `dislike_id`, `dislike_desc`)" . $query_tail . ";";
+			$result = mysqli_query($conn, $query)
+			or die ("Couldn't execute insert dislike query.");
 		}
 
 	}
@@ -56,7 +155,6 @@ else
 	header("location: LogIn.php");
 
 ?>
-
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -96,28 +194,69 @@ else
 	</div>
 </div>
 <div id="content">
-	<h3><?= $f_name . " " . $l_name ?></h3>
+	<h3><?= htmlspecialchars($f_name) . " " . htmlspecialchars($l_name)?></h3>
 	<div class="section">
 			<h3>Details</h3>
 			<p>
 			<form name="Details" method="post" id="Details"  onsubmit="" >
 				<div class="row requiredRow">
 					<label for="Sex">I am:</label>
-					<input type="radio" name="gender" value="m" checked> Male
-					<input type="radio" name="gender" value="f"> Female<br><br>
+					<input type="radio" name="gender" value="m" <?=$sexMale?>>Male
+					<input type="radio" name="gender" value="f"<?=$sexFemale?>>Female<br><br>
 				</div>
 				<div class="row requiredRow">
 					<label for="Seeking">Interested in:</label>
-					<input type="radio" name="seeking" value="m" checked> Male
-					<input type="radio" name="seeking" value="f"> Female<br><br>
+					<input type="radio" name="seeking" value="m" <?=$seekingMale?>>Male
+					<input type="radio" name="seeking" value="f" <?=$seekingFemale?>>Female<br><br>
 				</div>
 
 				<div class="row requiredRow">
 					<label for="Bio">Bio</label>
-					<textarea name="bio" input id="bio" type="text"  title="" />Say something about yourself</textarea><br><br>
+					<textarea name="bio" input id="bio" type="text"  title="" /><?=htmlspecialchars($bio)?> </textarea><br><br>
 
 				</div>
 
+				<div class="row">
+					<label for="Like1">Like 1</label>
+					<input id="Like1" name="Like1" type="text" title="" value="<?=htmlspecialchars($like[0])?>"/>
+				</div>
+				<div class="row">
+					<label for="Like2">Like 2</label>
+					<input id="Like2" name="Like2" type="text" title=""  value="<?=htmlspecialchars($like[1])?>"/>
+				</div>
+				<div class="row">
+					<label for="Like3">Like 2</label>
+					<input id="Like3" name="Like3" type="text" title=""  value="<?=htmlspecialchars($like[2])?>"/>
+				</div>
+				<div class="row">
+					<label for="Like4">Like 4</label>
+					<input id="Like4" name="Like4" type="text" title=""  value="<?=htmlspecialchars($like[3])?>"/>
+				</div>
+				<div class="row">
+					<label for="Like5">Like 5</label>
+					<input id="Like5" name="Like5" type="text" title=""  value="<?=htmlspecialchars($like[4])?>"/>
+				</div>
+<br><br>
+				<div class="row">
+					<label for="Dislike 1">Dislike 1</label>
+					<input id="Dislike1" name="Dislike1" type="text" title=""  value="<?=htmlspecialchars($dislike[0])?>"/>
+				</div>
+			<div class="row">
+				<label for="Dislike 2">Dislike 2</label>
+				<input id="Dislike2" name="Dislike2" type="text" title=""  value="<?=htmlspecialchars($dislike[1])?>"/>
+			</div>
+			<div class="row">
+				<label for="Dislike 3">Dislike 3</label>
+				<input id="Dislike3" name="Dislike3" type="text" title=""  value="<?=htmlspecialchars($dislike[2])?>"/>
+			</div>
+			<div class="row">
+				<label for="Dislike 4">Dislike 4</label>
+				<input id="Dislike4" name="Dislike4" type="text" title=""  value="<?=htmlspecialchars($dislike[3])?>"/>
+			</div>
+			<div class="row">
+				<label for="Dislike 5">Dislike 5</label>
+				<input id="Dislike5" name="Dislike5" type="text" title=""  value="<?=htmlspecialchars($dislike[4])?>"/>
+			</div>
 
 				<div class="row">
 					<input type="submit" value="Update" />
@@ -136,8 +275,6 @@ else
 			<br />
 			<span class="caption">Profile</span>
 		</div>
-		</p>
-		</p>
 
 
 	</div>
