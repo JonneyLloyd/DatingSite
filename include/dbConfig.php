3996648c6526492c454485870c315d5cbe46db0a
JonneyLloyd <jonneylloyd@yahoo.co.uk>
2016-04-14 12:26:07 +0100

    Database now connects to CSIS

--- include/dbConfig.php
+++ include/dbConfig.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "";
-$serv_username = "root";
-$serv_password = "";
-$dbname = "group17db";
-$conn = new mysqli($servername, $serv_username, $serv_password, $dbname, $port = 3306)
+$servername = "193.1.101.7";
+$serv_username = "group17";
+$serv_password = "bc7wzT8L9";
+$dbname = "group17DB";
+$conn = new mysqli($servername, $serv_username, $serv_password, $dbname, $port = 3307)
 or die ("Couldn't connect to server.");
